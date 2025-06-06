3b19923749bf4e73de15b9de508bfa3f00ae05f9
211G0370 <211G0370@rcarbonifera.tecnm.mx>
2025-06-05 20:13:20 -0600

    first commit

---
+++ RestaurantHamburguesa-Web/Areas/Waiter/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantHamburguesa_Web.Areas.Waiter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Ticket()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestaurantHamburguesa-Web/Areas/Waiter/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantHamburguesa_Web.Areas.Waiter.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
